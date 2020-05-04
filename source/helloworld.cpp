b037b60a7a101ce09897e7e2a020b8be81b33446
Klara Wichmann <klara@imagomat.de>
2020-05-04 17:50:26 +0200

    Aufgabe 1.3 hinzugefügt

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 
+/**
+ * Kleinstes gemeinsames Vielfaches von a und b finden
+ */
+  int kgv (int a, int b) {
+  int min = a < b ? a : b;
+  int max = a < b ? b : a;
+
+  for (int i = max; ; i += max) {
+    if (i % min == 0)
+      return i;
+    }
+  }
+
+/**
+ * Kleinste Zahl die durch 1-20 teilbar ist
+ */
+  int smallestNum () {
+    int max = 20;
+    int result = kgv(1, 2);
+    for (int i = 3; i <= max; i++) {
+      result = kgv(result, i);
+    }
+    return result;
+  }
+
+
 int main()
 {
-  std::cout << "Hello, New World!\n";
-  return 0;
+
+  //Kleinste durch 1 - 20 teilbare Zahl
+  std::cout << smallestNum () << "\n";
+
 }
